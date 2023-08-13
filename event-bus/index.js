6d65d80b4c34fe1fe54aa7dac436c582abef36ad
Ashrf <ashrftvm1@gmail.com>
2023-08-13 18:55:25 +0400

    implemented event bus

---
+++ event-bus/index.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const axios = require("axios");
+
+const app = express();
+app.use(bodyParser.json());
+
+app.post("/events", (req, res) => {
+  const content = req.body;
+
+  axios.post("http://localhost:4000/events", content).catch((e) => {
+    console.log("error", e);
+  });
+  axios.post("http://localhost:4001/events", content).catch((e) => {
+    console.log("error", e);
+  });
+  axios.post("http://localhost:4002/events", content).catch((e) => {
+    console.log("error", e);
+  });
+});
+
+app.listen(4005, () => {
+  console.log("Listening on 4005");
+});
