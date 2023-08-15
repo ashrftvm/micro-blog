7cb1c685ae3dff82a31caa1907bc6001de040a87
Ashrf <ashrftvm1@gmail.com>
2023-08-15 21:26:07 +0400

    updated to render comments from props

--- client/src/CommentList.jsx
+++ client/src/CommentList.jsx
@@ -1,20 +1,7 @@
-import React, { useEffect, useState } from "react";
+import React from "react";
 import axios from "axios";
 
-const CommentList = ({ postID }) => {
-  const [comments, setComments] = useState([]);
-
-  useEffect(() => {
-    const fetchComments = async () => {
-      const res = await axios.get(
-        `http://localhost:4001/posts/${postID}/comments`
-      );
-      setComments(res.data);
-      console.log("hello");
-    };
-    fetchComments();
-  }, [postID]);
-
+const CommentList = ({ comments }) => {
   const renderedComments = comments.map((comment) => {
     return <li key={comment.id}>{comment.comment}</li>;
   });
